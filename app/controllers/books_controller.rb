f91791a3e483c2d33082bdce74b92d4cb78e5459
sayakamik <138635063+sayakamik@users.noreply.github.com>
2023-09-12 04:09:24 +0000

    [Initial Commit2] 課題3 Bookers2-2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,17 @@
 class BooksController < ApplicationController
-  
-  
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+
+  end
+
+  def index
+    @book1 = Book.new
+    @book = Book.find(params[:id])
+    
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
 end
